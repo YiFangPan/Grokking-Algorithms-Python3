f7cd6c10c998f42705c5e8a459d92b24dbca7846
YiFangPan <sber13@gmail.com>
2020-05-14 16:08:51 +0800

    Add Book Solution. 4-9, Practice 4.1

--- CH4/ArraySumRecursive.py
+++ CH4/ArraySumRecursive.py
@@ -1,8 +1,19 @@
-def sum(arr):
-    if len(arr) == 1:
-        return arr[0]
+# 4-9, Practice 4.1
+
+# def sum(arr):
+#     if len(arr) == 1:
+#         return arr[0]
+#     else:
+#         return arr.pop(0) + sum(arr)
+
+# Book Solution
+def sum(list):
+    if list == []:
+        return 0
     else:
-        return arr.pop(0) + sum(arr)
+        # list[1:] => slice, from index 1 to end
+        return list[0] + sum(list[1:])
+
 
 test_array = [2, 4, 6]
 
