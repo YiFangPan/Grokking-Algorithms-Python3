4d8bb73d6d3af8c24d1ae0e76963a2b81ffab6ae
YiFangPan <sber13@gmail.com>
2020-05-16 16:39:27 +0800

    # Step 2. BFS - Use deque. 6-15

--- CH6/BFS.py
+++ CH6/BFS.py
@@ -1,5 +1,7 @@
 # Breadth-First Search(BFS)
 
+from collections import deque
+
 # Graph 6-12
 # Step 1. Use Hash store graph
 graph = {}
@@ -12,5 +14,20 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
+# mango seller's name leatest word is 'm'
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+# Step 2. BFS - Use deque
+search_queue = deque()
+search_queue += graph["you"]
+
+while search_queue:
+    person = search_queue.popleft()
+    if person_is_seller(person):
+        print(person + " is a mango seller!")
+        break
+    else:
+        search_queue += graph[person]
 
 
