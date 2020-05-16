82a8a28389af534abaa673906961a9830c1291c6
YiFangPan <sber13@gmail.com>
2020-05-16 16:23:39 +0800

    BFS-Step 1. Use Hash store graph. 6-12

---
+++ CH6/BFS.py
@@ -0,0 +1,16 @@
+# Breadth-First Search(BFS)
+
+# Graph 6-12
+# Step 1. Use Hash store graph
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+
