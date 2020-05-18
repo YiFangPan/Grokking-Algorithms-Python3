b8ba5702530a7d137eac0a11183534659200ef67
YiFangPan <sber13@gmail.com>
2020-05-18 23:23:29 +0800

    get lowest cost path and print path. 7-24

--- CH7/Dijkstras.py
+++ CH7/Dijkstras.py
@@ -32,16 +32,57 @@ parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
-# Step4. prepare processed hash table
-processed = {}
+# Step4. prepare processed array
+processed = []
 
 # Step5. 7-31
-def find_lowest_cost_node(costs):
+def find_lowest_cost_node():
     lowest_cost = maximum_float()
     lowest_cost_node = None
     for node in costs:
         cost = costs[node]
-        if cost < lowest_cost:
+        if cost < lowest_cost and node not in processed:
             lowest_cost = cost
             lowest_cost_node = node
     return lowest_cost_node
+
+# Step6. 7-24
+def find_lowest_cost_path():
+    node = find_lowest_cost_node()
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.append(node)
+        node = find_lowest_cost_node()
+
+
+def find_child_by_partent_node(node):
+    for n in parents.keys():
+        if n == node:
+            return parents[n]
+    return None
+
+
+def print_path():
+    path = []
+    path.append("fin")
+    node = parents["fin"]
+    while node is not None:
+        path.append(node)
+        node = find_child_by_partent_node(node)    
+    path.append("start")
+    path.reverse()
+    print(path[1:])
+
+
+find_lowest_cost_path()
+print_path()
+    
+
+
+
