97431d6d0330dd9aa55a2465c889a76ad0315497
YiFangPan <sber13@gmail.com>
2020-05-18 22:40:36 +0800

    Dijkstra's Algorithm. 7-21

---
+++ CH7/Dijkstras.py
@@ -0,0 +1,18 @@
+# 7-21
+
+# Step1. define weighted graph
+graph = {}
+
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 5
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
