0765e8f895da995c71177a9a23050a1e77ee6265
YiFangPan <sber13@gmail.com>
2020-05-18 22:53:07 +0800

    find_lowest_cost_node. 7-31

--- CH7/Dijkstras.py
+++ CH7/Dijkstras.py
@@ -1,5 +1,8 @@
 # 7-21
 
+def maximum_float():
+    return float("inf")
+
 # Step1. define weighted graph
 graph = {}
 
@@ -17,18 +20,28 @@ graph["b"]["fin"] = 5
 graph["fin"] = {}
 
 # Step2. define cost hash table
-infinity = float("inf")
+infinity = maximum_float()
 costs = {}
 costs["a"] = 6
 costs["b"] = 5
 costs["fin"] = infinity
 
-#Step3. define parent hash table
+# Step3. define parent hash table
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
-#Step4. prepare processed hash table
+# Step4. prepare processed hash table
 processed = {}
 
+# Step5. 7-31
+def find_lowest_cost_node(costs):
+    lowest_cost = maximum_float()
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
